658acb90e5d25329a765a34340a1ed1b5c2f8459
sebastian.popa <sebastian.popa@softvision.com>
2024-01-07 23:43:08 +0200

    Implementation of Project 12 using SwiftData

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -6,16 +6,56 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var showingUpcomingOnly = false
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
+            .navigationTitle("Users")
+            .toolbar {
+                Button("Add Samples", systemImage: "plus") {
+                    try? modelContext.delete(model: User.self)
+                    
+                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                    let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+                    
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
+                }
+                
+                Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                    showingUpcomingOnly.toggle()
+                }
+                
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by Name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                SortDescriptor(\User.joinDate)
+                            ])
+                        Text("Sort by Join Date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name)
+                            ])
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
--- SwiftDataProject/UsersView.swift
+++ SwiftDataProject/UsersView.swift
@@ -6,13 +6,57 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct UsersView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    
+    @Query var users: [User]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(users) { user in
+            HStack {
+                Text(user.name)
+                
+                Spacer()
+                
+                Text(String(user.unwrappedJobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
+        }
+        .onAppear(perform: addSample)
+    }
+    
+    init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]){
+        _users = Query(filter: #Predicate<User> { user in
+            user.joinDate >= minimumJoinDate
+        }, sort: sortOrder)
+    }
+    
+    func addSample() {
+        
+        try? modelContext.delete(model: User.self, where: #Predicate { user in
+            user.name == "Piper Chapman"
+        })
+        
+        let user1 = User(name: "Piper Chapman", city: "New York", joinDate: .now)
+        let job1 = Job(name: "Organize sock drawer", priority: 3)
+        let job2 = Job(name: "Make plans with Alex", priority: 4)
+        
+        modelContext.insert(user1)
+        
+        user1.jobs?.append(job1)
+        user1.jobs?.append(job2)
     }
 }
 
 #Preview {
-    UsersView()
+    UsersView(minimumJoinDate: .now, sortOrder: [SortDescriptor(\User.name)])
+        .modelContainer(for: User.self)
 }
